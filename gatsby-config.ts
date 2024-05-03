63e5de5c9e9183b90212283267101acc3b87417b
Leo Vargas <leu1607@hotmail.com>
2024-05-03 19:09:58 -0300

    initial repo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,11 +5,8 @@ const config: GatsbyConfig = {
     title: `teste-gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
@@ -21,6 +18,11 @@ const config: GatsbyConfig = {
       "path": "./src/images/"
     },
     __key: "images"
+  }, {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      trackingIds: ["GA-TRACKING_ID"]
+    }
   }]
 };
 
