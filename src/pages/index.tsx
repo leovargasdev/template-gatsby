4f275134b3d8806dbdff9933a023d6555fc79552
Leo Vargas <leu1607@hotmail.com>
2024-05-05 16:25:59 -0300

    config eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
-import * as styles from '../styles/home.module.scss'
+// import * as styles from '../styles/home.module.scss'
 
 const IndexPage = () => (
-  <div className={styles.container}>
+  <div className="{styles.container}">
     <h1>olar pessoas</h1>
-    <StaticImage src="../images/print.png" alt="A dinosaur" className={styles.img} />
+    <StaticImage
+      src="../images/print.png"
+      alt="A dinosaur"
+      className="{styles.img}"
+    />
   </div>
 )
 
